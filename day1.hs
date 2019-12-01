0f2a97c3193f685892f22e4912b2597784a3d6d4
Dione <dionemorales@outlook.com>
2019-12-01 18:13:45 +1100

    Solutions for day 1 part 1 & 2

---
+++ day1.hs
@@ -0,0 +1,13 @@
+
+calculateModuleFuel :: Integer -> Integer
+calculateModuleFuel mass = (div mass 3) - 2
+
+mainPart1 = do
+    input <- readFile "input/day1.txt"
+    let modules = map read (lines input)
+    print . sum . map calculateModuleFuel $ modules
+
+mainPart2 = do
+    input <- readFile "input/day1.txt"
+    let modules = map read (lines input) 
+    print . sum . map (sum . drop 1 . takeWhile (> 0) . iterate calculateModuleFuel) $ modules
